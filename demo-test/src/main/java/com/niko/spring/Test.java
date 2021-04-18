f76937fcd0f446100050ed2c0705a42ce4d40bff
Niko <nikomao89@mail.com>
2021-04-18 15:06:14 +0800

    v0.2

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -2,6 +2,6 @@ package com.niko.spring;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("add github ssh link");
+        System.out.println("add github ssh link-v0.2");
     }
 }
