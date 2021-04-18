eac380fad32af26e146e90cc64a13f538ceb9f91
niko-mao <nikomao89@gmail.com>
2021-04-18 09:38:35 +0800

    add 2 sout

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -3,8 +3,8 @@ package com.niko.spring;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello testing...");
-        System.out.println("hello testing...");
-        System.out.println("hello testing...");
-        System.out.println("hello testing...");
+        System.out.println("hello testing1...");
+        System.out.println("hello testing2...");
+        System.out.println("hello testing3...");
     }
 }
