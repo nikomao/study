1fdef7e137347d7bd8aa1512e8773c6269222b9b
Niko Mao <nikomao@users.noreply.github.com>
2021-04-18 09:34:57 +0800

    Update Test.java

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello testing...");
         System.out.println("hello testing...");
+        System.out.println("edit to github website.");
     }
 }
