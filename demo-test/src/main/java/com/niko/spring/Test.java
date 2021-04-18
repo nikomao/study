9c64ec029f6887df5a3bb933a1f9389299b96f18
Niko Mao <nikomao@users.noreply.github.com>
2021-04-18 14:54:34 +0800

    Update Test.java
    change codes -v0.1

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -2,10 +2,6 @@ package com.niko.spring;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello testing...");
-        System.out.println("hello testing1...");
-        System.out.println("hello testing2...");
-        System.out.println("hello testing3...");
-        System.out.println("edit to website...");
+        System.out.println("change master branch code -v0.1");
     }
 }
