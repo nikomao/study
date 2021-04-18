0494006c83ae8e20500f2f5621e9dc6f6405257d
Niko Mao <nikomao@users.noreply.github.com>
2021-04-18 15:30:38 +0800

    Update Test.java
    v0.7

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -3,6 +3,6 @@ package com.niko.spring;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("change master branch code -v0.5");
+        System.out.println("change master branch code -v0.7 -github website.");
     }
 }
