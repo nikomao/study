e3f3436db3282edea2265a2045505f05829d73d1
niko-mao <nikomao89@gmail.com>
2021-04-18 09:37:11 +0800

    add 2 sout

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello testing...");
         System.out.println("hello testing...");
-        System.out.println("edit to github website.");
+        System.out.println("hello testing...");
+        System.out.println("hello testing...");
     }
 }
