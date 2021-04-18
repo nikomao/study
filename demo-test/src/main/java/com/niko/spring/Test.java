01740132529afae58a4ad9ab36c62e1adcbefba9
Niko <nikomao89@mail.com>
2021-04-18 15:38:09 +0800

    update to v0.5

--- demo-test/src/main/java/com/niko/spring/Test.java
+++ demo-test/src/main/java/com/niko/spring/Test.java
@@ -4,6 +4,5 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("change master branch code -v0.7 -github website.");
-        System.out.println("add github ssh link-v0.5-niko-dev");
     }
 }
